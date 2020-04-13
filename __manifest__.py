fda83462e0ab7e1a76106fdfb118519bbe35b703
samping <289966969@qq.com>
2020-04-13 18:05:39 +0800

    git

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+   'name': "Approval Flow by HSP",
+
+   'summary': """
+    Approve according to HR personnel structure""",
+
+   'description': """
+    
+   """,
+
+   'author': "HSP",
+   'website': "http://www.einfo-tech.com",
+   'category': 'Tools',
+   'version': '1.0',
+
+   'depends': ['base','hr','hr_org_chart'],
+
+   'data': [
+        "security/ir.model.access.csv",
+		   "views/templates.xml", 
+         "views/demo.xml",
+         "security/res.groups.xml", 
+   ],
+   'qweb': [
+        'static/src/xml/hr_org_chart.xml',
+    ],
+
+   "installable":True,
+
+}
